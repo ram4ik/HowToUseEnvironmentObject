121a8cc2e9ab162726d2479f0062741479e2e039
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-30 09:02:14 +0200

    Initial Commit

---
+++ HowToUseEnvironmentObject/HowToUseEnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseEnvironmentObjectApp.swift
+//  HowToUseEnvironmentObject
+//
+//  Created by ramil on 30.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseEnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
